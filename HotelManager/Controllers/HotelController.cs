70e9cc939a54c42ee01f617ca7c4306b02e69ffd
Arthur Bassi <arthur.winiarski.ranger@outlook.com>
2024-03-09 20:08:48 -0400

    First commit

---
+++ HotelManager/Controllers/HotelController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
